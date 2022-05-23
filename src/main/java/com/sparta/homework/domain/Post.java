608d6e832381b898783d37c2c314d06cdb637c69
DoyupK <gltlvl12@gmail.com>
2022-05-23 17:08:56 +0900

    GET 요청시 패스워드 정보 ignore 추가

--- src/main/java/com/sparta/homework/domain/Post.java
+++ src/main/java/com/sparta/homework/domain/Post.java
@@ -1,5 +1,6 @@
 package com.sparta.homework.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -24,6 +25,7 @@ public class Post extends Timestamped { // 생성,수정 시간을 자동으로 
     private String contents;
 
     @Column(nullable = false)
+    @JsonIgnore
     private String password;
 
     public Post(String title, String username, String contents, String password) {
