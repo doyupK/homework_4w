0503cab17a3a8b4b9cde3cc370233d8aae6d9a06
DoyupK <gltlvl12@gmail.com>
2022-07-04 11:52:56 +0900

    testsets

--- src/main/java/com/sparta/homework/controller/HomeController.java
+++ src/main/java/com/sparta/homework/controller/HomeController.java
@@ -23,7 +23,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        // hello 22
+        // hello 22333333333333
         if (userDetails != null) {
             model.addAttribute("username", userDetails.getUsername());
         }
